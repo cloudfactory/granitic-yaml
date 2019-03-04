27c284b15edcf3577d576f61050e987648582544
benhalstead <benhalstead@gmail.com>
2019-03-04 10:40:04 +0000

    Package alias on generated project

--- cmd/grnc-yaml-project/main.go
+++ cmd/grnc-yaml-project/main.go
@@ -53,17 +53,14 @@ func writeComponentsFile(compDir string, pg *generate.ProjectGenerator) {
 
 func writeMainFile(w *bufio.Writer, projectPackage string) {
 
-	changePackageComment := "  //Change to a non-relative path if you want to use 'go install'"
-
 	w.WriteString("package main\n\n")
-	w.WriteString("import \"github.com/graniticio/granitic-yaml/v2\"\n")
+	w.WriteString("import gy \"github.com/graniticio/granitic-yaml/v2\"\n")
 	w.WriteString("import \"")
 	w.WriteString(projectPackage)
 	w.WriteString("/bindings\"")
-	w.WriteString(changePackageComment)
 	w.WriteString("\n\n")
 	w.WriteString("func main() {\n")
-	w.WriteString("\tgranitic_yaml.StartGraniticWithYaml(bindings.Components())\n")
+	w.WriteString("\tgy.StartGraniticWithYaml(bindings.Components())\n")
 	w.WriteString("}\n")
 
 }
